09749abac8165ab2ff9d419ace6ef30a7313de5c
pdkz <pidekazu@gmail.com>
2016-09-15 17:19:10 +0900

    Update coloredconsole.py

--- coloredconsole.py
+++ coloredconsole.py
@@ -1,21 +1,34 @@
 import os
 import sys
-import json
 import ctypes
-import urllib2
+
+class ConsoleColor(object):
+    CLEAR   = 'clear'
+    WHITE   = 'white'
+    BLACK   = 'black'
+    BLUE    = 'blue'
+    GREEN   = 'green'
+    RED     = 'red'
+    YELLOW  = 'yellow'
+    MAGENTA = 'magenta'
+    CYAN    = 'cyan'
 
 class ColoredConsole(object):
-    if os.name == 'nt' :
-        """
-           Color definition for Windows
-        """
+    colors = None
+    def __init__(self, color):
+        ColoredConsole.colors = color
 
+    @classmethod
+    def printf(cls, col, *objects, **kwargs):
+        pass
+
+class ColoredConsoleWin32(ColoredConsole):
+    if os.name == 'nt':
         STD_OUTPUT_HANDLE = -11
 
         """
             Foreground color definition
         """
-
         FG_BLACK = 0x00
         FG_BLUE  = 0x01
         FG_GREEN = 0x02
@@ -31,7 +44,6 @@ class ColoredConsole(object):
         """
             Background color definition
         """
-
         BG_BLACK = 0x00
         BG_BLUE  = 0x10
         BG_GREEN = 0x20
@@ -44,74 +56,70 @@ class ColoredConsole(object):
 
         BG_INTENSITY = 0x80
 
-        fg_colors = { 'black'  : FG_BLACK,
-                      'blue'   : FG_BLUE | FG_INTENSITY,
-                      'green'  : FG_GREEN | FG_INTENSITY,
-                      'red'    : FG_RED | FG_INTENSITY,
-                      'white'  : FG_WHITE | FG_INTENSITY,
-                      'cyan'   : FG_CYAN | FG_INTENSITY,
-                      'magenta': FG_MAGENTA | FG_INTENSITY,
-                      'yellow' : FG_YELLOW | FG_INTENSITY,
+        fg_colors = { ConsoleColor.CLEAR   : FG_WHITE,
+                      ConsoleColor.BLACK   : FG_BLACK,
+                      ConsoleColor.BLUE    : FG_BLUE | FG_INTENSITY,
+                      ConsoleColor.GREEN   : FG_GREEN | FG_INTENSITY,
+                      ConsoleColor.RED     : FG_RED | FG_INTENSITY,
+                      ConsoleColor.WHITE   : FG_WHITE | FG_INTENSITY,
+                      ConsoleColor.CYAN    : FG_CYAN | FG_INTENSITY,
+                      ConsoleColor.MAGENTA : FG_MAGENTA | FG_INTENSITY,
+                      ConsoleColor.YELLOW  : FG_YELLOW | FG_INTENSITY,
                      }
 
-        bg_colors = { 'black'  : BG_BLACK,
-                      'blue'   : BG_BLUE | BG_INTENSITY,
-                      'green'  : BG_GREEN | BG_INTENSITY,
-                      'red'    : BG_RED | BG_INTENSITY,
-                      'white'  : BG_WHITE | BG_INTENSITY,
-                      'cyan'   : BG_CYAN | BG_INTENSITY,
-                      'magenta': BG_MAGENTA | BG_INTENSITY,
-                      'yellow' : BG_YELLOW | BG_INTENSITY,
+        bg_colors = { ConsoleColor.BLACK   : BG_BLACK,
+                      ConsoleColor.BLUE    : BG_BLUE | BG_INTENSITY,
+                      ConsoleColor.GREEN   : BG_GREEN | BG_INTENSITY,
+                      ConsoleColor.RED     : BG_RED | BG_INTENSITY,
+                      ConsoleColor.WHITE   : BG_WHITE | BG_INTENSITY,
+                      ConsoleColor.CYAN    : BG_CYAN | BG_INTENSITY,
+                      ConsoleColor.MAGENTA : BG_MAGENTA | BG_INTENSITY,
+                      ConsoleColor.YELLOW  : BG_YELLOW | BG_INTENSITY,
                      }
         std_out_handler = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
 
-    else :
-        # ANSI Color
-        colors = {
-            'clear' : '\033[0m',
-            'black' : '\033[30m',
-            'red'   : '\033[31m',
-            'green' : '\033[32m',
-            'yellow': '\033[33m',
-            'blue'  : '\033[34m',
-            'purple': '\033[35m',
-            'cyan'  : '\033[36m',
-            'white' : '\033[37m'
-        }
+    def __init__(self):
+        ColoredConsole.__init__(self, ColoredConsoleWin32.colors)
 
     @classmethod
-    def blue(cls, string, width=0, align=None):
-        string = format_string(string, width, align)
-        return '%s%s' % (cls.colors['blue'], string)
+    def printf(cls, col, *objects, **kwargs):
+        cls.set_color(col)
+
+        sep = kwargs.get('sep', ' ')
+        out = kwargs.get('file', sys.stdout)
+        out.write(sep.join(objects))
+        cls.set_color(ConsoleColor.CLEAR)
 
     @classmethod
-    def green(cls, string, width=0, align=None):
-        string = format_string(string, width, align)
-        return '%s%s' % (cls.colors['green'], string)
-
-    @classmethod
-    def white(cls, string, width=0, align=None):
-        string = format_string(string, width, align)
-        return '%s%s' % (cls.colors['white'], string)
-
-    @classmethod
-    def _write_console(string):
-        sys.stdout.write('%s' % string)
-
-    @classmethod
-    def get_colored_string(cls, string, col='white', width=0, align=None):
-        string = format_string(string, width, align)
-        return '%s%s' % (cls.colors[col], string)
-
-    @classmethod
-    def set_color(cls, fg_colorname = 'white', bg_colorname = 'black'):
-        """
-           Windows only
-        """
-        fg_color = cls.fg_colors[fg_colorname]
-        bg_color = cls.bg_colors[bg_colorname]
+    def set_color(cls, fg_colorname=ConsoleColor.CLEAR, bg_colorname=ConsoleColor.BLACK):
+        fg_color = cls.fg_colors.get(fg_colorname, ConsoleColor.CLEAR)
+        bg_color = cls.bg_colors.get(bg_colorname, ConsoleColor.BLACK)
 
         ctypes.windll.kernel32.SetConsoleTextAttribute(cls.std_out_handler, fg_color | bg_color)
+
+class ColoredConsoleLinux(ColoredConsole):
+    # ANSI Color
+    colors = {
+        ConsoleColor.CLEAR  : '\033[0m',
+        ConsoleColor.BLACK  : '\033[30m',
+        ConsoleColor.RED    : '\033[31m',
+        ConsoleColor.GREEN  : '\033[32m',
+        ConsoleColor.YELLOW : '\033[33m',
+        ConsoleColor.BLUE   : '\033[34m',
+        ConsoleColor.MAGENTA: '\033[35m',
+        ConsoleColor.CYAN   : '\033[36m',
+        ConsoleColor.WHITE  : '\033[37m'
+    }
+
+    def __init__(self):
+        ColoredConsole.__init__(self, ColoredConsoleLinux.colors)
+
+    @classmethod
+    def printf(cls, col, *objects, **kwargs):
+        sep = kwargs.get('sep', ' ')
+        out = kwargs.get('file', sys.stdout)
+        out.write(cls.colors.get(col, ConsoleColor.CLEAR) + sep.join(objects))
+        out.write(cls.colors.get(ConsoleColor.CLEAR))
 
 def format_string(string, width, align):
     if width <= 0 :
@@ -119,3 +127,14 @@ def format_string(string, width, align):
     fmt = '{:^%d}' % width
     string = fmt.format(string)
     return string
+
+class ColoredConsoleFactory(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def create():
+        if os.name == 'nt':
+            return ColoredConsoleWin32
+        else :
+            return ColoredConsoleLinux
